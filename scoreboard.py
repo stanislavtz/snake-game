6540de6ad63e068c71a75f5a58727d4b3deee3cc
stzekov <stanislav.tzekov@dxc.com>
2024-09-01 11:01:31 +0300

    Add high score keeper

--- scoreboard.py
+++ scoreboard.py
@@ -4,10 +4,23 @@ ALIGNMENT = "center"
 FONT = ('Courier', 18, 'bold')
 
 
+def get_high_score():
+	with open("high-score.txt", mode="r") as file:
+		score_list = file.read()
+		high_score = score_list.split(" ")[-1]
+		return int(high_score)
+
+
+def save_high_score(score):
+	with open("high-score.txt", mode="w") as file:
+		file.write(f"High_score = {score}")
+
+
 class ScoreBoard(Turtle):
 	def __init__(self):
 		super().__init__()
-		self.scores = 0
+		self.score= 0
+		self.high_score = get_high_score()
 		self.color("white")
 		self.penup()
 		self.goto(0, 260)
@@ -15,11 +28,14 @@ class ScoreBoard(Turtle):
 
 	def display_scores(self):
 		self.clear()
-		self.write(arg=f"Scores: {self.scores}", align=ALIGNMENT, font=FONT)
+		self.write(arg=f"Score: {self.score}. High-score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
 	def increase_scores(self):
-		self.scores += 1
+		if self.high_score == self.score:
+			self.high_score += 1
+		self.score += 1
 
 	def game_over(self):
 		self.goto(0, 0)
 		self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+		save_high_score(self.high_score)
